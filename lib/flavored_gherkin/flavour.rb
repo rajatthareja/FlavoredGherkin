b7f2989e48d3997f7de637c274e1895247cf681a
Rajat Thareja <rajat.thareja.1990@gmail.com>
2017-11-06 15:50:56 +0530

    flavour doc and, js and css v cdn

--- lib/flavored_gherkin/flavour.rb
+++ lib/flavored_gherkin/flavour.rb
@@ -5,6 +5,13 @@ module FlavoredGherkin
   # Base for Flavored Gherkin Flavours
   #
   class Flavour
+    ##
+    # Parser Feature Files and Returns List of Gherkin Documents
+    #
+    # @param [Array] feature_files List of feature files
+    #
+    # @return [Array] List of Gherkin Documents
+    #
     def get_gherkins(feature_files)
       parser = Gherkin::Parser.new
       feature_files.map do |file|
